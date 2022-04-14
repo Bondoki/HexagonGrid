d8657b61efb406d97d05c0cc9244ed7d1ad41108
bondoki <bondoki@gmx.de>
2022-04-14 22:52:58 +0200

    * clean up and adding hovering star

--- IsometricTilesHexagon.cpp
+++ IsometricTilesHexagon.cpp
@@ -12,7 +12,6 @@
 
 #include "Hex.h"
 
-// Override base class with your custom functionality
 class IsometricTilesHexagon : public olc::PixelGameEngine
 {
 public:
@@ -22,6 +21,7 @@ public:
 	}
 
 private:
+        // Total passed time
         float fTotalElapsedTime;
   
 	// Number of tiles in world
@@ -33,23 +33,24 @@ private:
         // Size of single tile graphic and offset to align them
 	olc::vi2d vTileSizeOffset = { 0, 10 };
 
-	// Where to place tile (0,0) on screen (in tile size steps)
-	//olc::vi2d vOrigin = { 1, 2 };
-        
         // Where to place tile (0,0) on screen (in screen pixel steps)
         olc::vi2d vOriginScreen = { 72*3, 46*3 };
 
 	// Sprite that holds all imagery
-	olc::Sprite *sprIsom = nullptr;
+	olc::Renderable  sprIsom;
 
-        // list to order for drawing
+        // List to order for drawing
 	std::list<Hex> hexlist;
         
         // HexTile to Value
         using HexMap=std::unordered_map<Hex, int,decltype(hexhash), decltype(hexequal)>;
         HexMap heights{HexMap(10,hexhash,hexequal)};
         
+        // Star-Sprite and shadow
+	olc::Renderable  sprStar;
         
+        // Board-style name
+        std::string sBoardName;
 
 public:
 	bool OnUserCreate() override
@@ -59,9 +60,13 @@ public:
                 srand (time(NULL));
 
 		// Load sprites used in demonstration
-		sprIsom = new olc::Sprite("assets/hextiles.png");
-
-		createBoard(2);
+		sprIsom.Load("assets/hextiles.png");
+                
+                sprStar.Load("assets/star.png");
+                
+                // we start with hexagonal board
+                sBoardName="";
+                createBoard(2);
                
 		return true;
 	}
@@ -77,6 +82,7 @@ public:
           switch(nr)
           {
             case 0: //Rectangle 1
+                    sBoardName="Rectangle 1";
                     for (int q = -3; q <= 3; q++)
                     { // flat top
                       int q_offset = floor(q/2.0); // or q>>1
@@ -93,6 +99,7 @@ public:
                     break;
     
             case 1: //Rectangle 2
+                    sBoardName="Rectangle 2";
                     for (int q = -3; q <= 3; q++)
                     { // flat top
                       int q_offset = floor((q+1)/2.0); 
@@ -109,6 +116,7 @@ public:
                     break;
     
             case 2: //Hexagon
+                    sBoardName="Hexagon";
                     dist = 4;
                     for (int q = -dist; q <= dist; q++)
                     {
@@ -123,6 +131,7 @@ public:
                     break;
                     
             case 3: //Parallelograms 1
+                    sBoardName="Parallelogram 1";
                     dist = 3;
                     for (int q = -dist; q <= dist; q++) 
                     {
@@ -138,6 +147,7 @@ public:
                     break;
                     
             case 4: //Parallelograms 2
+                    sBoardName="Parallelogram 2";
                     dist = 2;
                     for (int r = -dist; r <= dist; r++)
                     {
@@ -153,6 +163,7 @@ public:
                     break;
                     
             case 5: // Parallelograms 3
+                    sBoardName="Parallelogram 3";
                     dist = 2;
                     for (int s = -dist; s <= dist; s++)
                     {
@@ -168,6 +179,7 @@ public:
                     break;
                     
             case 6: // Triangle 1
+                    sBoardName="Triangle 1";
                     dist = 6;
                     for (int q = 0; q <= dist; q++)
                     {
@@ -180,6 +192,7 @@ public:
                     break;
                     
             case 7: // Triangle 2
+                    sBoardName="Triangle 2";
                     dist = 6;
                     for (int q = 0; q <= dist; q++)
                     {
@@ -232,23 +245,6 @@ public:
 		// Get Mouse in world
 		olc::vi2d vMouse = { GetMouseX(), GetMouseY() };
 		
-		// Work out active cell
-		//olc::vi2d vCell = { vMouse.x / vTileSize.x, vMouse.y / vTileSize.y };
-
-		// Work out mouse offset into cell
-		//olc::vi2d vOffset = { vMouse.x % vTileSize.x, vMouse.y % vTileSize.y };
-
-		// Sample into cell offset colour
-		//olc::Pixel col = sprIsom->GetPixel(3 * vTileSize.x + vOffset.x, vOffset.y);
-
-		// Work out selected cell by transforming screen cell
-		
-			//(vCell.y - vOrigin.y) + (vCell.x - vOrigin.x),
-			//(vCell.y - vOrigin.y) - (vCell.x - vOrigin.x) 
-			//(-1./3 * point.x  +  sqrt(3)/3 * point.y) / size
-			//q,r
-                         //std::floor(4.0 / 3.0 * ((1.0*vMouse.x-vOriginScreen.x) / (vTileSize.x-vTileSizeOffset.x))) ,
-                         //std::floor(-2.0 / 3.0 * ((1.0*vMouse.x-vOriginScreen.x) / (vTileSize.x-vTileSizeOffset.x)) + 2.0*1.7320508075688/3.0 * ((1.0*vMouse.y-vOriginScreen.y+vTileSizeOffset.y) / (vTileSize.y-vTileSizeOffset.y)))
 		
                          double qfrac = 4.0 / 3.0 * ((1.0*vMouse.x-vOriginScreen.x - 0.5*(vTileSize.x-vTileSizeOffset.x)) / (vTileSize.x-vTileSizeOffset.x));
                          double rfrac = -2.0 / 3.0 * ((1.0*vMouse.x-vOriginScreen.x - 0.5*(vTileSize.x-vTileSizeOffset.x)) / (vTileSize.x-vTileSizeOffset.x)) + 2.0*1.7320508075688/3.0 * ((1.0*vMouse.y-vOriginScreen.y-0.5*(vTileSize.y-vTileSizeOffset.y)) / (vTileSize.y-vTileSizeOffset.y));
@@ -278,38 +274,24 @@ public:
                          qi, ri
                 };
 
-		// "Bodge" selected cell by sampling corners
-		//if (col == olc::RED) vSelected += {-1, +0};
-		//if (col == olc::BLUE) vSelected += {+0, -1};
-		//if (col == olc::GREEN) vSelected += {+0, +1};
-		//if (col == olc::YELLOW) vSelected += {+1, +0};
-
-		// Handle mouse click to toggle if a tile is visible or not
+		
+		// Handle mouse left click to switch tile
 		if (GetMouse(0).bPressed)
 		{
 			// Guard array boundary
                          auto got = heights.find (Hex(vSelected.x,vSelected.y,-vSelected.x-vSelected.y));
                   
                         if ( got != heights.end() )
-			//if (vSelected.x >= 0 && vSelected.x < vWorldSize.x && vSelected.y >= 0 && vSelected.y < vWorldSize.y)
                         {
-				
-                                //heights.at(Hex(x,y,-x-y))++
-                                //++heights.at(Hex(vSelected.x,vSelected.y,-vSelected.x-vSelected.y)) %=6;
-                                int value = heights.at(Hex(vSelected.x,vSelected.y,-vSelected.x-vSelected.y));
+				int value = heights.at(Hex(vSelected.x,vSelected.y,-vSelected.x-vSelected.y));
                                 
                                 value = (value%9)+1;
                                 heights.at(Hex(vSelected.x,vSelected.y,-vSelected.x-vSelected.y)) = value;
-                                
-                                //heights.at(Hex(vSelected.x,vSelected.y,-vSelected.x-vSelected.y))++;
-                                //heights.at(Hex(vSelected.x,vSelected.y,-vSelected.x-vSelected.y)) %=7;
-                                //heights.at(Hex(vSelected.x,vSelected.y,-vSelected.x-vSelected.y))++;
-                                //std::cout << "modify" << std::endl;
                         }
                         
 		}
                 
-                // Handle mouse click to toggle tile - right click
+                // Handle mouse right click to toggle tile - creation/destruction
 		if (GetMouse(1).bPressed)
 		{
 			// Guard array boundary
@@ -340,8 +322,7 @@ public:
 		// Labmda function to convert "world" (offset-array) coordinate into screen space
 		auto ToScreen = [&](int x, int y)
 		{			
-                        
-			return olc::vi2d
+                        return olc::vi2d
 			{
                                 0.5*(vTileSize.x -vTileSizeOffset.x) * 3.0/2.0 *         (x)+vOriginScreen.x,
                                 0.5*((vTileSize.y-vTileSizeOffset.y) * 1.7320508075688 *((y) + 0.5 * ((x)&1)))+vOriginScreen.y 
@@ -358,99 +339,15 @@ public:
                                 0.5*((vTileSize.y-vTileSizeOffset.y) * (1.7320508075688/2.0 * (q) + 1.7320508075688*r))+vOriginScreen.y
 			};
 		};
-                /*3.0 / 2.0, 0.0, sqrt(3.0) / 2.0, sqrt(3.0)
-                Point hex_to_pixel(Layout layout, Hex h) {
-    const Orientation& M = layout.orientation;
-    double x = (M.f0 * h.q + M.f1 * h.r) * layout.size.x;
-    double y = (M.f2 * h.q + M.f3 * h.r) * layout.size.y;
-    return Point(x + layout.origin.x, y + layout.origin.y);*/
-
-
-		
-		// Draw World - has binary transparancy so enable masking
-		//SetPixelMode(olc::Pixel::MASK);
-                SetPixelMode(olc::Pixel::ALPHA);
-		// (0,0) is at top, defined by vOrigin, so draw from top to bottom
-		// to ensure tiles closest to camera are drawn last
-		/*for (int y = 0; y < vWorldSize.y; y++)
-		{
-			for (int x = 0; x < vWorldSize.x; x+=2)
-			{
-				// Convert cell coordinate to world space
-				olc::vi2d vWorld = ToScreen(x, y);
-				
-				switch(pWorld[y*vWorldSize.x + x])
-				{
-				case 0:
-					// Invisble Tile
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 0 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
-					break;
-				case 1:
-					// Visible Tile
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 1 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
-					break;
-				case 2:
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 2 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
-					break;
-				case 3:
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 0 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
-					break;
-				case 4:
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 1 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
-					break;
-				case 5:
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 2 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
-					break;
-				}			
-			}
-			
-			for (int x = 1; x < vWorldSize.x; x+=2)
-			{
-				// Convert cell coordinate to world space
-				olc::vi2d vWorld = ToScreen(x, y);
-				
-				switch(pWorld[y*vWorldSize.x + x])
-				{
-				case 0:
-					// Invisble Tile
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 0 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
-					break;
-				case 1:
-					// Visible Tile
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 1 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
-					break;
-				case 2:
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 2 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
-					break;
-				case 3:
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 0 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
-					break;
-				case 4:
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 1 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
-					break;
-				case 5:
-					DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 2 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
-					break;
-				}			
-			}
-		}
-		*/
-                /*
-                for (int r = 0; r < vWorldSize.y; r++) {
-                   for (int q = 0; q < vWorldSize.x; q++) {
-                    olc::vi2d vWorld = AxialToScreen(q, r);
-                    DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 1 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
-					
-                    
-                  }
-                }
-                */
                 
+		
+                // draw tiles
                 for ( auto it=hexlist.cbegin(); it!=hexlist.cend(); ++it )
                 {
                   int q=(*it).get_q(); int r=(*it).get_r(); int s=(*it).get_s();
                   
                   auto got = heights.find (Hex(q,r,s));
+                  
                   
                   if ( got != heights.end() )
                   {
@@ -470,40 +367,42 @@ public:
 				{
 				case 0:
 					// Invisble Tile
-					DrawPartialSprite(vWorld.x+moveXSelected, vWorld.y+moveYSelected, sprIsom, 0 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
+					DrawPartialDecal({vWorld.x+moveXSelected, vWorld.y+moveYSelected}, sprIsom.Decal(), {0 * vTileSize.x, 0}, {vTileSize.x, vTileSize.y});
 					break;
 				case 1:
 					// Visible Tile
-					DrawPartialSprite(vWorld.x+moveXSelected, vWorld.y+moveYSelected, sprIsom, 1 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
+					DrawPartialDecal({vWorld.x+moveXSelected, vWorld.y+moveYSelected}, sprIsom.Decal(), {1 * vTileSize.x, 0}, {vTileSize.x, vTileSize.y});
 					break;
 				case 2:
-					DrawPartialSprite(vWorld.x+moveXSelected, vWorld.y+moveYSelected, sprIsom, 2 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
+					DrawPartialDecal({vWorld.x+moveXSelected, vWorld.y+moveYSelected}, sprIsom.Decal(), {2 * vTileSize.x, 0}, {vTileSize.x, vTileSize.y});
 					break;
                                 case 3:
-					DrawPartialSprite(vWorld.x+moveXSelected, vWorld.y+moveYSelected, sprIsom, 3 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
+					DrawPartialDecal({vWorld.x+moveXSelected, vWorld.y+moveYSelected}, sprIsom.Decal(), {3 * vTileSize.x, 0}, {vTileSize.x, vTileSize.y});
 					break;
                                 case 4:
-					DrawPartialSprite(vWorld.x+moveXSelected, vWorld.y+moveYSelected, sprIsom, 4 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
+					DrawPartialDecal({vWorld.x+moveXSelected, vWorld.y+moveYSelected}, sprIsom.Decal(), {4 * vTileSize.x, 0}, {vTileSize.x, vTileSize.y});
 					break;
                                         
 				case 5:
-					DrawPartialSprite(vWorld.x+moveXSelected, vWorld.y+moveYSelected, sprIsom, 0 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
+					DrawPartialDecal({vWorld.x+moveXSelected, vWorld.y+moveYSelected}, sprIsom.Decal(), {0 * vTileSize.x, 1 * vTileSize.y}, {vTileSize.x, vTileSize.y});
 					break;
 				case 6:
-					DrawPartialSprite(vWorld.x+moveXSelected, vWorld.y+moveYSelected, sprIsom, 1 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
+					DrawPartialDecal({vWorld.x+moveXSelected, vWorld.y+moveYSelected}, sprIsom.Decal(), {1 * vTileSize.x, 1 * vTileSize.y}, {vTileSize.x, vTileSize.y});
 					break;
 				case 7:
-					DrawPartialSprite(vWorld.x+moveXSelected, vWorld.y+moveYSelected, sprIsom, 2 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
+					DrawPartialDecal({vWorld.x+moveXSelected, vWorld.y+moveYSelected}, sprIsom.Decal(), {2 * vTileSize.x, 1 * vTileSize.y}, {vTileSize.x, vTileSize.y});
 					break;
                                 case 8:
-					DrawPartialSprite(vWorld.x+moveXSelected, vWorld.y+moveYSelected, sprIsom, 3 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
+					DrawPartialDecal({vWorld.x+moveXSelected, vWorld.y+moveYSelected}, sprIsom.Decal(), {3 * vTileSize.x, 1 * vTileSize.y}, {vTileSize.x, vTileSize.y});
 					break;
                                         
                                  case 9:
-					DrawPartialSprite(vWorld.x+moveXSelected, vWorld.y+moveYSelected, sprIsom, 4 * vTileSize.x, 1 * vTileSize.y, vTileSize.x, vTileSize.y);
+					DrawPartialDecal({vWorld.x+moveXSelected, vWorld.y+moveYSelected}, sprIsom.Decal(), {4 * vTileSize.x, 1 * vTileSize.y}, {vTileSize.x, vTileSize.y});
 					break;
                                 
 				}
+				
+                        
                   }
                   
                   // Draw selected
@@ -513,51 +412,35 @@ public:
                   {
                       olc::vi2d vWorld = AxialToScreen(vSelected.x,vSelected.y);
                       
-                      
-                      // Invisble Tile
-                      //DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 0 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
-                      DrawPartialSprite(vWorld.x, vWorld.y+5*std::sin(fTotalElapsedTime*5), sprIsom, 0 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
-                      
-                    
+                      DrawPartialDecal({vWorld.x, vWorld.y+5*std::sin(fTotalElapsedTime*5)}, sprIsom.Decal(), {0 * vTileSize.x, 0 * vTileSize.y}, {vTileSize.x, vTileSize.y});
                   }
                 }
-                }
                 
-                
-                /*
-                for ( auto it=hexmap.cbegin(); it!=hexmap.cend(); ++it )
+                  // Draw star
                 {
-                  olc::vi2d vWorld = AxialToScreen((*it).get_q(), (*it).get_r());
-                  DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 1 * vTileSize.x, 0, vTileSize.x, vTileSize.y);	
+                      int moveXSelected=0;
+                      int moveYSelected=0;
+                      
+                      // highlight selected hexagon
+                      if ( hexequal(Hex(vSelected.x,vSelected.y,-vSelected.x-vSelected.y), Hex(0,0,0)) )
+                      {
+                         moveXSelected=0.0;
+                         moveYSelected=5*std::sin(fTotalElapsedTime*5);
+                      }
+                          olc::vi2d vWorld = AxialToScreen(0,0);
+                      
+                          DrawPartialDecal({vWorld.x+vTileSize.x/6-3*std::sin(fTotalElapsedTime*5), vWorld.y-0.1*vTileSize.y+moveYSelected}, sprStar.Decal(), {1 * 46, 0}, {46, 44},  { 1.0f+0.15*std::sin(fTotalElapsedTime*5),1.0f }, olc::WHITE);
+                          
+                          DrawPartialDecal({vWorld.x+vTileSize.x/6, vWorld.y+5*std::sin(fTotalElapsedTime*5)-0.75*vTileSize.y}, sprStar.Decal(), {0 * 40, 0}, {46, 44},  { 1.0f,1.0f }, olc::WHITE);
+                          
                 }
-                
-                */
-                /*
-                
-                for ( auto it=hexset.cbegin(); it!=hexset.cend(); ++it )
-                {
-                  olc::vi2d vWorld = AxialToScreen((*it).get_q(), (*it).get_r());
-                  DrawPartialSprite(vWorld.x, vWorld.y, sprIsom, 1 * vTileSize.x, 0, vTileSize.x, vTileSize.y);	
+                  
                 }
-                */
-		// Draw Selected Cell - Has varying alpha components
-		//SetPixelMode(olc::Pixel::ALPHA);
-
-		// Convert selected cell coordinate to world space
-		//olc::vi2d vSelectedWorld = ToScreen(vSelected.x, vSelected.y);
-
-		// Draw "highlight" tile
-		//DrawPartialSprite(vSelectedWorld.x, vSelectedWorld.y, sprIsom, 0 * vTileSize.x, 0, vTileSize.x, vTileSize.y);
-
-		// Go back to normal drawing with no expected transparency
-		SetPixelMode(olc::Pixel::NORMAL);
-
-		// Draw Hovered Cell Boundary
-		//DrawRect(vCell.x * vTileSize.x, vCell.y * vTileSize.y, vTileSize.x, vTileSize.y, olc::RED);
-				
+                	
 		// Draw Debug Info
 		DrawString(4, 4, "Mouse   : " + std::to_string(vMouse.x) + ", " + std::to_string(vMouse.y), olc::BLACK);
-		DrawString(4, 24, "Selected: " + std::to_string(vSelected.x) + ", " + std::to_string(vSelected.y), olc::BLACK);
+		DrawString(4, 14, "Selected: " + std::to_string(vSelected.x) + ", " + std::to_string(vSelected.y), olc::BLACK);
+		DrawString(4, 24, "Style: " + sBoardName, olc::BLACK);
 		
                 // Graceful exit if user is in full screen mode
                 return !GetKey(olc::Key::ESCAPE).bPressed;
